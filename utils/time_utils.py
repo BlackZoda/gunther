b9856718e359240c6f2c74b9276bc32b534dfdb5
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2025-04-21 14:45:42 +0200

    formatted the time output, and made it slightly prettier

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -7,15 +7,15 @@ from config import OPEN_AI
 oslo_tz = pytz.timezone('Europe/Oslo')
 la_tz = pytz.timezone('America/Los_Angeles')
 la_tz_33 = datetime.now(la_tz) - relativedelta(years=92)
-oslo_time = datetime.now(oslo_tz).strftime('%Y-%m-%d %H:%M:%S')
-la_time = datetime.now(la_tz).strftime('%Y-%m-%d %H:%M:%S')
-la_time_33 = la_tz_33.strftime('%Y-%m-%d %H:%M:%S')
+oslo_time = datetime.now(oslo_tz).strftime('_%Y-%m-%d_\t%H:%M')
+la_time = datetime.now(la_tz).strftime('_%Y-%m-%d_\t%H:%M')
+la_time_33 = la_tz_33.strftime('_%Y-%m-%d_\t%H:%M')
 la_date_33 = la_tz_33.strftime("%B %d, %Y")
 
 openai.api_key = OPEN_AI
     
 def return_time():
-    return f"🇳🇴 Time in Norway: {oslo_time}\n🇺🇸 Time in Trumpistan: {la_time_33}"
+    return f"🇳🇴 **Time in Norway:**\t\t\t{oslo_time}\n🇺🇸 **Time in Trumpistan:**\t{la_time_33}"
 
 async def generate_history():
     prompt = f"Give me some relevant historical events that happened on or around {la_date_33}. Preferably related to fascism and/or nazi-germany, and write a fun comparison to the current political situation in the USA."
